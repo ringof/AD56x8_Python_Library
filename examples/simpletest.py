5e069d255f1c4dd8eac8970adbae5cbb9a14cff9
ringof <davegoncalves@gmail.com>
2019-02-23 20:36:47 -0500

    Simpletest ramp extended to all DAC channels.

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -1,20 +1,28 @@
 import Adafruit_GPIO.SPI as SPI
 from AD56x8 import AD56x8
-import time
+
+
+DAC_CH = ['DAC_A', 'DAC_B', 'DAC_C', 'DAC_D', 'DAC_E', 'DAC_F', 'DAC_G', 'DAC_H']
 
 # BeagleBone Black hardware SPI configuration.
 SPI_PORT   = 1
 SPI_DEVICE = 0
-dac = AD56x8.AD56x8('AD5628-1',spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+dac = AD56x8.AD56x8('AD5628-1', spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-#dac.reset()
+dac.reset()
 dac.internal_ref_mode('ON')
-dac.LDAC_mode(True, 'DAC_A')
+
+# Configure all channels for SW LDAC
+for ch in DAC_CH:
+    dac.LDAC_mode('SW', ch)
 
 counter = 0
 
 while True:
-    dac.write_to_Input_Reg_update_all('ALL_DAC', counter)
+    for ch in DAC_CH:
+        dac.write_to_Input_Regs(ch, counter)
+        dac.update_DAC_Regs(ch)
+
     voltage = 2*(dac.VREF)*(counter/(2**dac.DATA_WIDTH))
     counter = (counter + 1) % ((2**dac.DATA_WIDTH)-1)
-    print("DAC A Voltage:", round(voltage, 3))
+    print("DAC Voltage:", round(voltage, 3))
